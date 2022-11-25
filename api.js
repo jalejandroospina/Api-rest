96d3a0f972aa144200b02868db9b8ea8dc70c582
Alejandro <jalejandroospina@ucundinamarca.edu.co>
2022-11-24 23:13:11 -0500

    public folder

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const PORT = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extenden:true}));
+app.use(express.static('public'));
 
 const server = app. listen(PORT,()=>
 {       // en escucha al servidor
